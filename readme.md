3709d11ca969ef953f0a3f0e8b2b94ed8238ed05
Ly Trong Nhan <lytro@sheridancollege.ca>
2025-03-04 16:16:44 -0500

    Initial change by Ly Trong Nhan

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Midterm Q2
+This is Q2 if the midterm
---
+++ test-project/src/main/java/ca/sheridan/App.java
@@ -0,0 +1,13 @@
+package ca.sheridan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App //Ly Trong Nhan
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
